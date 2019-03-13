a1ac2b78a65c5b0bd03fa0e7b77666c6521ed097
xd23fe39 <xd23fe39@yahoo.de>
2019-03-13 23:55:10 +0100

    Test erweitert

--- test/install/test-install.sh
+++ test/install/test-install.sh
@@ -11,8 +11,17 @@ PWD=$(pwd)
 BASEDIR=$(cd `dirname $0`; cd ..; pwd)
 cd ${BASEDIR}
 
+function _check() {
+    if [[ $? != 0 ]]; then
+        echo "FAIL: $1"
+        exit 2
+    fi
+}
+
 echo $(cat /etc/issue) | cut -d ' ' -f 1-3
 hostname -A
 uname -a
 id -a pibox
+_check id
 ls -l /var/pibox
+check /var/pibox 
